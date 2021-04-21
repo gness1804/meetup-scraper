be31a00c67a23878d6d7af0711782a8a9dd19d5a
gness1804 <graham.nessler@gmail.com>
2021-04-20 20:02:28 -0500

    got links showing up on page for meetup topic

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ require('dotenv').config();
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const { writeFile } = require('fs').promises;
+
+const [, , query] = process.argv;
+
+if (!query) throw new Error('Error: query argument required. Example: "tennis".');
+
+// TODO: make city and state dynamic via inputs
+const url = `https://www.meetup.com/find/?allMeetups=false&keywords=${query}&radius=50&userFreeform=Austin%2C+TX&mcId=z73301&mcName=Austin%2C+TX&sort=recommended&eventFilter=all`;
+
+(async () => {
+  let html;
+  try {
+    const res = await fetch(url);
+    html = await res.text();
+  } catch (error) {
+    /* eslint-disable-next-line no-console */
+    console.error(`Failed to retrieve recipe: ${error}.`);
+    process.exit(1);
+  }
+
+  const $ = cheerio.load(html);
+
+  const links = $('.groupCard--photo');
+  links.each(async (i, link) => {
+    const { href } = link.attribs;
+  })
+}
+)()
